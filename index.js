2bbf67e0a8fd43ab47c0b404041e71f97dff65de
Thiago Bezerra de Carvalho <112219943+thiaguinhodopagode@users.noreply.github.com>
2023-04-12 17:54:57 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+/*Parte do exercício feito!*/
+
 let dividendo = prompt("Você quer dividir quanto?:")
 let divisor = prompt("Com quantos?:")
 quociente = dividendo / divisor
+    
+alert(`${dividendo} ÷ ${divisor} = ${quociente}`)
 
-alert(`${dividendo} ÷ ${divisor}: ${quociente}`)
+/*------------------------------------------------------*/
